bdfdb12b5f544497d576992bed04ab6f6c2301de
Wigwa <lucywigwa7@gmail.com>
2020-05-03 21:52:08 +0300

    function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var Male = {Sunday:"Kwasi", Monday:"Kwadwo", Tuesday:"Kwabena",Wednesday:"Kwaku", Thursday:"Yaw", Friday:"Afua", Saturday:"Ama"};
 
 var Female = {Sunday:"Akosua", Monday:"Adwoa", Tuesday:"Abenaa", Wednesday:"Akua", Thursday:"Yaa", Friday:"Afua", Saturday:"Ama"};
+var day = function();
