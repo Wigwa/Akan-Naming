d089bc460b16c8e913f4da1b12a9e30dfad45b79
Wigwa <lucywigwa7@gmail.com>
2020-05-04 13:30:57 +0300

    javascript

--- index.js
+++ index.js
@@ -1,77 +1,60 @@
+//declaring variables
+var century, year, month, dayOfMonth, years, gender;
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+var femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 
-function input() {
-    var century = document.getElementById("century").value;
-    var cc = parseInt(century)
-    var month = document.getElementById("month").value;
-    var mm = parseInt(month);
-    var day = document.getElementById("day").value;
-    var dd = parseInt(day);
-    var year = document.getElementById("year").value;
-    var yy = parseInt(year);
-    var cc = (yy - 1) / 100 + 1;
+//get imput
+function storeInput(){
+//century = parseInt(document.getElementById("century").value);
+year = parseInt(document.getElementById("year").value);
+month = parseInt(document.getElementById("month").value);
+dayOfMonth = parseInt(document.getElementById("dayofmonth").value);
+century = document.getElementById("year").value.slice(0,2);
+century = parseInt(century);
+years = document.getElementById("year").value.slice(2,4);
+years = parseInt(years);
 
-    var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
-    var boyNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var girlNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    if (document.getElementById("gender").checked) {
-        var gender = 'male';
-        
-    }
-    else {
-        var gender = 'female';
-    }
-    if (mm < 1 || mm > 12 || mm == 2 && dd > 29) {
-        alert("invalid month");
-    }
-    else if (dd < 1 || dd > 31) {
-        alert("invalid date");
-    }
+//checking for Validity and calculating day of the week
+if (month<1 || month>12){
+  alert('Invalid Month');
+}
+else if (dayOfMonth<1 || dayOfMonth>31) {
+  alert('Invalid Day');
+}
+else {
+  var dayOfWeek = Math.floor(( ( (century/4) -2*century-1) + ((5*years/4) ) + ((26*(month+1)/10)) + dayOfMonth ) % 7 );
+  return dayOfWeek;
+}
+}
 
+//getting Gender
+function getGender(){
+  var selectedGender = document.getElementById('inputGroupSelect01').value;
+if (selectedGender==1) {
+   gender = "male";
+  }
+  else if (selectedGender==2) {
+   gender = "female"
+  }
+  else {
+    alert("Select Gender");
+  }
+  return gender;
+}
 
-
-    else if (Math.round(result) == 2 && gender === 'male') {
-        document.getElementById("display").innerHTML = ("You were born on Tuesday ,your akan name is " + boyNames[2]);
-    }
-    else if (Math.round(result) == 3 && gender === 'male') {
-        document.getElementById("display").innerHTML = ("You were born on Wednesday ,your akan name is " + boyNames[3]);
-    }
-    else if (Math.round(result) == 4 && gender === 'male') {
-        document.getElementById("display").innerHTML = ("You were born on Thursday,your akan name is " + boyNames[4]);
-    }
-    else if (Math.round(result) == 5 && gender === 'male') {
-        document.getElementById("display").innerHTML = ("You were born on Friday,your akan name is " + boyNames[5]);
-    }
-    else if (Math.round(result) == 6 && gender === 'male') {
-        document.getElementById("display").innerHTML = ("you were born on Saturday,your akan name is " + boyNames[6]);
-    }
-    
-    else if (Math.round(result) == 0 && gender === 'male') {
-        document.getElementById("display").innerHTML = ("You were born on Sunday,your akan name is " + boyNames[0]);
-    }
-    else if (Math.round(result) == 1 && gender === 'female') {
-        document.getElementById("display").innerHTML = ("You were born on monday ,your akan name is " + girlNames[1]);
-    }
-    else if (Math.round(result) == 2 && gender === 'female') {
-        document.getElementById("display").innerHTML = ("You were born on Tuesday,your akan name is " + girlNames[2]);
-    }
-    else if (Math.round(result) == 3 && gender === 'female') {
-        document.getElementById("display").innerHTML = ("You were born on Wednesday,your akan name is " + girlNames[3]);
-    }
-    else if (Math.round(result) == 4 && gender === 'female') {
-        document.getElementById("display").innerHTML = ("You were born on Thursday,your akan name is " + girlNames[4]);
-    }
-
-    else if (Math.round(result) == 5 && gender === 'female') {
-        document.getElementById("display").innerHTML = ("You were born on Friday,your akan name is " + girlNames[5]);
-    }
-    else if (Math.round(result) == 6 && gender === 'female') {
-        document.getElementById("display").innerHTML = ("You were born on Saturday,your akan name is " + girlNames[6]);
-    }
-    else if (Math.round(result) == 0 && gender === 'female') {
-        document.getElementById("display").innerHTML = ("You were born on Sunday,your akan name is " + girlNames[0]);
-    }
-
-    else {
-        alert("invalid data please");
-    }
+//finding akan names
+function akanNameFind(){
+  var newDayOfWeek = storeInput();
+  var newGender = getGender();
+  if(newGender==="male"){
+    document.getElementById("answer").innerHTML=("You are a Male born on " +days[newDayOfWeek] +" and your Akan name is. " +maleNames[newDayOfWeek]);
+  }
+  else if (newGender==="female") {
+    document.getElementById("answer").innerHTML=("You are a Female born on " +days[newDayOfWeek] +" and your Akan name is. " +femaleNames[newDayOfWeek]);
+  }
+  else {
+    document.getElementById("answer").innerHTML=("Check your input please");
+  }
+  document.querySelectorAll("#year, #month, #dayofmonth").value = "";
 }
