9323f7650fc904c78424fd1813776f8e8f358033
Wigwa <lucywigwa7@gmail.com>
2020-05-04 12:03:14 +0300

    name

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function textInput() {
-  var boyName = 
+  var boyName = [
+    Sunday="Kwasi", Monday="kwadwo", Tuesday="Kwabena", Wednesday="Kwaku", Thursday="Yaw", Friday="Kofi", Saturday="Kwame"];
+    var girlName = [
+      Sunday="Akosua", Monday="Kwadwo", Tuesday="Abenaa", Wednesday="Akua", Thursday="Yaa", Friday="Afua", Saturday="Ama" 
+    ];
 }
